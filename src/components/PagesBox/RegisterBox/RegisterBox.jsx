a9136194b23692a8512df72b017f9a50fca18e61
DevWaleedS <eng.waleed.salah@outlook.com>
2023-03-29 16:40:57 +0200

    COMMIT

--- src/components/PagesBox/RegisterBox/RegisterBox.jsx
+++ src/components/PagesBox/RegisterBox/RegisterBox.jsx
@@ -221,7 +221,7 @@ const RegisterBox = () => {
 			'@media (max-width: 768px)': {
 				top: phonenumberError === 'The phonenumber field is required when user type is store.' ? '-10px' : '20px',
 			},
-			'@media (max-width: 390px)': {
+			'@media (max-width: 480px)': {
 				top: phonenumberError === 'The phonenumber field is required when user type is store.' ? '-34px' : '20px',
 			},
 			

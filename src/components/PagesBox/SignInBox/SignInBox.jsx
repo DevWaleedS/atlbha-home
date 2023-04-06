8c6d69ebd6440537a5575e9a35d36a9dbda73f33
DevWaleedS <eng.waleed.salah@outlook.com>
2023-04-07 01:23:33 +0200

    create user info component

--- src/components/PagesBox/SignInBox/SignInBox.jsx
+++ src/components/PagesBox/SignInBox/SignInBox.jsx
@@ -25,9 +25,8 @@ const SignInBox = () => {
 		};
 		axios.post('https://backend.atlbha.com/api/loginapi', data).then((res) => {
 			if (res?.data?.success === true && res?.data?.data?.status === 200) {
-				setCookie('access_token', res?.data?.data?.token);
-				// put this code when you finsh 
-				// , { domain: 'atlbha.com' , path:'/'}
+				setCookie('access_token', res?.data?.data?.token , { domain: 'atlbha.com' , path:'/'});
+				
 				<Navigate to='/' />;
 				// window.location.href = 'http://store.atlbha.com'; // url dashboard tajer
 			} else {
